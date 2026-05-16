a2bf698e2fb033ecd71fb171a6bb195978770da5
neon_0039 <uranium124427@gmail.com>
2026-05-16 14:30:33 +0900

    Update bot.js

--- bot.js
+++ bot.js
@@ -940,7 +940,7 @@ function generateMarkov(words, brain) {
         return candidate;
     };
 
-    const mm = Math.floor(Math.random() * (17 - 5 + 1)) + 15;
+    const mm = Math.floor(Math.random() * (13 - 5 + 1)) + 15;
     let generated = "";
     let current_word = pickNextWord(words);
 
